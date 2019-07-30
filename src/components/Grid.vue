f7f486850ece2de8aed60f8d5ba3dd4294fd2d55
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-07-30 10:49:31 -0400

    Create basic grid block layout

---
+++ src/components/Grid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="grid">
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#grid {
+  display: grid;
+  grid-template: 200px 200px 200px / 200px 200px 200px;
+  & > div {
+    border: 5px solid;
+  }
+}
+</style>
